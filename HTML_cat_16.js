3e1786284890a45a51d76b16a3969210d73eea1b
Tanya <bykovskaya-t@mail.ru>
2024-11-25 21:33:09 +0300

    cat 16

---
+++ HTML_cat_16.js
@@ -0,0 +1,2 @@
+      <p>Everyone loves <a href="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg">cute cats</a> online!</p>
+      <p>See more <a target="_blank" href="https://freecatphotoapp.com">cat photos</a> in our gallery.</p>// JavaScript source code
