1a0c1d9c522c3f8bde26a4608649e36d005bca12
Tanya <bykovskaya-t@mail.ru>
2024-11-26 22:14:46 +0300

    cat 26

---
+++ HTMK_cat_26.js
@@ -0,0 +1,4 @@
+<figure>
+    <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="A slice of lasagna on a plate.">
+        <figcaption>Cats love lasagna.</figcaption>
+</figure>// JavaScript source code
