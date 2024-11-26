15405231eb988848a0f7c611f6e1dd0d34e276df
Tanya <bykovskaya-t@mail.ru>
2024-11-26 22:11:10 +0300

    cat 24 25

---
+++ HTMK_cat_24.js
@@ -0,0 +1,6 @@
+       <ul>
+          <li>cat nip</li>
+          <li>laser pointers</li>
+          <li>lasagna</li>
+        </ul>
+        <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="A slice of lasagna on a plate.">// JavaScript source code
---
+++ HTML_cat_25.js
@@ -0,0 +1,4 @@
+
+<figure>
+    <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="A slice of lasagna on a plate.">
+</figure>// JavaScript source code
