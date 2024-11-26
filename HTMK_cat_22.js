ebaa0d818d231c10b73e39fe0bc221c3adf11dc6
Tanya <bykovskaya-t@mail.ru>
2024-11-26 22:04:50 +0300

    cat 21_22

---
+++ HTMK_cat_22.js
@@ -0,0 +1,3 @@
+       <h2>Cat Lists</h2>
+        <h3>Things cats love:</h3>
+        <ul></ul>// JavaScript source code
---
+++ HTML_cat_21.js
@@ -0,0 +1,4 @@
+<section>
+    <h2>Cat Lists</h2>
+    <h3>Things cats love:</h3>
+</section>// JavaScript source code
